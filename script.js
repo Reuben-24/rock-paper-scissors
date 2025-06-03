46e5eb23834ff0b3238918f2f961617ed44de48c
Reuben-24 <reubengfaltiska@gmail.com>
2025-06-03 11:33:17 +1000

    Code html and js file

---
+++ script.js
@@ -0,0 +1,82 @@
+function getComputerChoice() {
+    const randomNumber = Math.random() * 3;
+    if (randomNumber < 1) {
+        return "rock";
+    }
+    else if (randomNumber < 2) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }   
+}
+
+function getHumanChoice() {
+    const message = "Enter your choice: ";
+    return prompt(message).trim().toLowerCase();
+}
+
+function logHumanWin() {
+    console.log("You Win!");
+}
+
+function logHumanLoss() {
+    console.log("You Lose!");
+}
+
+function logTie() {
+    console.log("Tie");
+}
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            logTie();
+        }
+        else if (computerChoice === "paper") {
+            logHumanLoss();
+            computerScore++;
+        }
+        else {
+            logHumanWin();
+            humanScore++;
+        }
+    }
+    else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            logHumanWin();
+            humanScore++;
+        }
+        else if (computerChoice === "paper") {
+            logTie();
+        }
+        else {
+            logHumanLoss();
+            computerScore++;
+        }
+    }
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            logHumanLoss();
+            computerScore++;
+        }
+        else if (computerChoice === "paper") {
+            logHumanWin();
+            humanScore++;
+        }
+        else {
+            logTie();
+        }
+    }
+}
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    for (i = 0; i < 5; i++)
+    {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+}
+
+playGame();
+
